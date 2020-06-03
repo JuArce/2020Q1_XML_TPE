a1984c2f78fb835a6ccfe00a422fde1b388e2e02
Roberto <robertojcatalan@gmail.com>
2020-06-03 11:27:03 -0300

    Modificaciones al script
    Todavia no funciona

--- tp.sh
+++ tp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://www.openstreetmap.org/api/0.6/map?bbox=-74.00,40.711,-73.99,40.73
+curl https://www.openstreetmap.org/api/0.6/map?bbox=-74.00,40.711,-73.99,40.73 > salida.xml
