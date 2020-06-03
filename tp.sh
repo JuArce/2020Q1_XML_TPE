779026747c7db4c4c748154aa72cddd080eec045
Roberto <robertojcatalan@gmail.com>
2020-06-03 20:23:24 -0300

    Script con los argumentos listos

--- tp.sh
+++ tp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo hello world
-curl "https://www.openstreetmap.org/api/0.6/map?bbox=-74.00,40.711,-73.99,40.73" > "data.xml"
+curl "https://www.openstreetmap.org/api/0.6/map?bbox=${1},${2},${3},${4}" > "data.xml"
+echo "hola"
