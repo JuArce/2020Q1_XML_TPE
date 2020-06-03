71569eeddb295cd7f1da89094e3053a75346e677
Roberto <robertojcatalan@gmail.com>
2020-06-03 20:43:46 -0300

    Problemas en el script

--- tp.sh
+++ tp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-curl "https://www.openstreetmap.org/api/0.6/map?bbox=${1},${2},${3},${4}" > "data.xml"
-echo "hola"
+$1 -lt $3 | bc 
+if [ $1 -lt $3 | -bc ]
+then
+    curl "https://www.openstreetmap.org/api/0.6/map?bbox=${1},${2},${3},${4}" > "data.xml"
+fi
+echo hola
