ace6d55d7050c2e9bd432496b389af7500b50a3e
Roberto <robertojcatalan@gmail.com>
2020-06-03 11:43:11 -0300

    C

--- tp.sh
+++ tp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo hello world
-curl https://openstreetmap.org/api/0.6/map?bbox=-74.00,40.711,-73.99,40.73, > salida.xml
+curl https://openstreetmap.org/api/0.6/map?bbox=-74.00,40.711,-73.99,40.73 > salida.xml
